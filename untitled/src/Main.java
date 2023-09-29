666f37209f3a0384d77b785a857a7a40324c09fa
nevina-bradley <129434576+nevina-bradley@users.noreply.github.com>
2023-09-29 12:34:30 -0400

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    list1 = {1, 3, 5, 7}
+    list2 = {1,2.8, 9}
+    list3 = {1}
+
+    for (i = 0; i < listNum.length; i++) {
+        list1Num = list1(i);
+    }
+    
+    for (i = 0; i < list1Num.length; i++) {
+        if (list1Num == list2Num) {
+            list1Num go into list3
+        }
+    }
+}
